1eda89c50e357bdd7a79958da1fe16170539e8f1
Yuri Bulavin <nebalama@gmail.com>
2020-08-28 19:50:21 +0300

    test 3

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -s /home/box/web/etc/stepic18.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/stepic18.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
