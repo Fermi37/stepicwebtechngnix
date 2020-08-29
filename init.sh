6e1338a06a5693824790ca8fb42a41d0324d2bb3
Yuri Bulavin <nebalama@gmail.com>
2020-08-29 17:03:19 +0300

    test4

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -s /home/box/web/etc/stepic191.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/stepic191.conf  /etc/nginx/sites-enabled/stepic191.conf
 sudo /etc/init.d/nginx restart
